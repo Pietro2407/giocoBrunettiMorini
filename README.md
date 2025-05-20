696f6b027e8b10843f2792840317fd2d63068645
Riccardo Morini <Riccardo Morini>
2025-05-20 09:24:23 +0200

    aggiunti prof e movimento casuale

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # giocoBrunettiMorini
+Lo scopo del gioco è arrivare alla fine del labirinto (casella rossa) entro il tempo specificato in basso a sinistra e senza farsi prendere dai nemici che si muovono all'interno del labirinto (i prof).
+Per muoversi nelle quattro direzioni basterà usare i tasti delle freccette.
+
+Se non riesci a raggiungere l'arrivo entro il limite di tempo o vieni catturato in automatico comparirà una schermata con scritto "HAI PERSO".
+
+Buon divertimento
--- src/giocoBrunettiMorini/Labirinto.java
+++ src/giocoBrunettiMorini/Labirinto.java
@@ -17,7 +17,7 @@ public class Labirinto extends Application {
     private static final int CELL_SIZE = 32;
     private static final int WIDTH = 30;
     private static final int HEIGHT = 27;
-    private static final int TIME_LIMIT = 30;
+    private static final int TIME_LIMIT = 45;
 
     private ImageView giocatore;
     private int xGiocatore = 1;
@@ -26,40 +26,46 @@ public class Labirinto extends Application {
     private int yArrivo = 25;
     private ImageView immagineFinale;
 
+    private ImageView nemico1;
+    private int xNemico1 = 5, yNemico1 = 5;
+
+
+    private ImageView nemico2;
+    private int xNemico2 = 16, yNemico2 = 20;
+
 
     private int tempoRimasto = TIME_LIMIT;
     private Text tempo;
 
     private final int[][] labirinto = {
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-        {1,0,0,0,1,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,0,0,1,0,0,0,1},
-        {1,0,1,0,1,0,1,1,0,1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1},
-        {1,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,0,1,0,0,0,0,0,1,0,1},
-        {1,0,1,1,1,1,0,1,1,1,1,1,0,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,0,1},
-        {1,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1},
-        {1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,0,1},
-        {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1},
-        {1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1},
-        {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,1},
-        {1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1},
-        {1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
-        {1,0,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1},
-        {1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1},
-        {1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1},
-        {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1},
-        {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1},
-        {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1},
-        {1,0,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1},
-        {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1},
-        {1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,0,1,1,1,1,1},
-        {1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1},
-        {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1},
-        {1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1},
-        {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1},
-        {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-    };
-
+    		{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    	    {1,0,0,1,0,0,0,1,0,1,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,1,0,1},
+    	    {1,0,1,1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,0,1},
+    	    {1,0,1,0,0,1,0,0,0,0,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,0,0,1,0,1},
+    	    {1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1,1,1,1,1,0,1},
+    	    {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,1,0,1},
+    	    {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1},
+    	    {1,0,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1},
+    	    {1,0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+    	    {1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1},
+    	    {1,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1},
+    	    {1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1},
+    	    {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1},
+    	    {1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,1},
+    	    {1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1},
+    	    {1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1},
+    	    {1,0,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1},
+    	    {1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1},
+    	    {1,0,1,0,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1},
+    	    {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1},
+    	    {1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,1},
+    	    {1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,1,1,0,1},
+    	    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
     @Override
     public void start(Stage stage) {
         Pane pannello = new Pane();
@@ -84,6 +90,27 @@ public class Labirinto extends Application {
         giocatore.setY(yGiocatore * CELL_SIZE);
         pannello.getChildren().add(giocatore);
 
+       
+        Image imgNemico1 = new Image(getClass().getResourceAsStream("immagini/nemico1.png"));
+        nemico1 = new ImageView(imgNemico1);
+        nemico1.setFitWidth(CELL_SIZE);
+        nemico1.setFitHeight(CELL_SIZE);
+        nemico1.setX(xNemico1 * CELL_SIZE);
+        nemico1.setY(yNemico1 * CELL_SIZE);
+        pannello.getChildren().add(nemico1);
+
+        
+        Image imgNemico2 = new Image(getClass().getResourceAsStream("immagini/nemico2.png"));
+        nemico2 = new ImageView(imgNemico2);
+        nemico2.setFitWidth(CELL_SIZE);
+        nemico2.setFitHeight(CELL_SIZE);
+        nemico2.setX(xNemico2 * CELL_SIZE);
+        nemico2.setY(yNemico2 * CELL_SIZE);
+        pannello.getChildren().add(nemico2);
+
+        Timeline movimentoNemici = new Timeline(new KeyFrame(Duration.millis(500), e -> muoviNemici()));
+        movimentoNemici.setCycleCount(Timeline.INDEFINITE);
+        movimentoNemici.play();
 
         final String[] direzione = {"giu"};
 
@@ -113,7 +140,6 @@ public class Labirinto extends Application {
                 giocatore.setX(xGiocatore * CELL_SIZE);
                 giocatore.setY(yGiocatore * CELL_SIZE);
 
-
                 int indicatore = switch (direzione[0]) {
                     case "destra" -> 6;
                     case "sinistra" -> 2;
@@ -134,27 +160,69 @@ public class Labirinto extends Application {
         stage.show();
     }
 
-    private void caricaLabirinto(Pane root) {
-        for (int y = 0; y < HEIGHT; y++) {
-            for (int x = 0; x < WIDTH; x++) {
-                if (labirinto[y][x] == 1) {
-                    Image fotoPavimento = new Image(getClass().getResourceAsStream("immagini/pavimento.png"));
-                    ImageView pavimento = new ImageView(fotoPavimento);
-                    pavimento.setX(x * CELL_SIZE);
-                    pavimento.setY(y * CELL_SIZE);
-                    root.getChildren().add(pavimento);
-                } else {
-                    Image fotoMuro = new Image(getClass().getResourceAsStream("immagini/muro.png"));
-                    ImageView muro = new ImageView(fotoMuro);
-                    muro.setX(x * CELL_SIZE);
-                    muro.setY(y * CELL_SIZE);
-                    root.getChildren().add(muro);
-                }
+    private void muoviNemici() {
+        muoviNemicoCasuale(nemico1, new int[]{xNemico1, yNemico1}, nemico -> {
+            xNemico1 = nemico[0];
+            yNemico1 = nemico[1];
+        });
+
+        muoviNemicoCasuale(nemico2, new int[]{xNemico2, yNemico2}, nemico -> {
+            xNemico2 = nemico[0];
+            yNemico2 = nemico[1];
+        });
+
+        controllaCollisioni();
+    }
+    
+    private void muoviNemicoCasuale(ImageView nemico, int[] posizione, java.util.function.Consumer<int[]> aggiornaPosizione) {
+        int x = posizione[0];
+        int y = posizione[1];
+
+        int[][] direzioni = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        java.util.List<int[]> direzioniValide = new java.util.ArrayList<>();
+
+        for (int[] d : direzioni) {
+            int newX = x + d[0];
+            int newY = y + d[1];
+            if (newX >= 0 && newX < WIDTH && newY >= 0 && newY < HEIGHT && labirinto[newY][newX] == 0) {
+                direzioniValide.add(d);
             }
         }
 
-        Image fotoarrivo = new Image(getClass().getResourceAsStream("immagini/arrivo.png"));
-        ImageView arrivo = new ImageView(fotoarrivo);
+        if (!direzioniValide.isEmpty()) {
+            int[] scelta = direzioniValide.get((int) (Math.random() * direzioniValide.size()));
+            int nuovoX = x + scelta[0];
+            int nuovoY = y + scelta[1];
+
+            aggiornaPosizione.accept(new int[]{nuovoX, nuovoY});
+            nemico.setX(nuovoX * CELL_SIZE);
+            nemico.setY(nuovoY * CELL_SIZE);
+        }
+    }
+
+
+    private void controllaCollisioni() {
+        if ((xGiocatore == xNemico1 && yGiocatore == yNemico1) ||
+            (xGiocatore == xNemico2 && yGiocatore == yNemico2)) {
+            mostraImmagineFinale("immagini/hai_perso.png");
+        }
+    }
+
+
+
+    private void caricaLabirinto(Pane root) {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                Image img = new Image(getClass().getResourceAsStream("immagini/" + (labirinto[y][x] == 1 ? "pavimento" : "muro") + ".png"));
+                ImageView cella = new ImageView(img);
+                cella.setX(x * CELL_SIZE);
+                cella.setY(y * CELL_SIZE);
+                root.getChildren().add(cella);
+            }
+        }
+
+        Image fotoArrivo = new Image(getClass().getResourceAsStream("immagini/arrivo.png"));
+        ImageView arrivo = new ImageView(fotoArrivo);
         arrivo.setX(xArrivo * CELL_SIZE);
         arrivo.setY(yArrivo * CELL_SIZE);
         root.getChildren().add(arrivo);
@@ -167,15 +235,15 @@ public class Labirinto extends Application {
     }
 
     private void aggiornaTimer() {
-        if (xGiocatore == xArrivo && yGiocatore == yArrivo) {
-        	return;
-        }
+        if (xGiocatore == xArrivo && yGiocatore == yArrivo) return;
+
         if (tempoRimasto <= 0) {
             mostraImmagineFinale("immagini/hai_perso.png");
         } else {
             tempo.setText("Tempo rimasto: " + (--tempoRimasto));
         }
     }
+
     private void mostraImmagineFinale(String percorsoImmagine) {
         Image imgFinale = new Image(getClass().getResourceAsStream(percorsoImmagine));
         immagineFinale = new ImageView(imgFinale);
@@ -184,13 +252,11 @@ public class Labirinto extends Application {
         immagineFinale.setX(0);
         immagineFinale.setY(0);
 
-      
         giocatore.setVisible(false);
         tempo.setVisible(false);
 
         ((Pane) giocatore.getParent()).getChildren().add(immagineFinale);
     }
-
 
     public static void main(String[] args) {
         launch(args);
